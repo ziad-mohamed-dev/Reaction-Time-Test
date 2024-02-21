9fb061286b33bbbf2814f42c819fee6811617053
Hunter-Smsm <143844320+Hunter-Smsm@users.noreply.github.com>
2024-02-21 20:36:43 +0200

    Reaction Time Test

---
+++ main.js
@@ -0,0 +1,25 @@
+let testBox = document.querySelector(".test");
+let h1 = document.querySelector("h1");
+let h4 = document.querySelector("h4");
+
+testBox.onclick = function test() {
+    h1.innerHTML = "Wait For Green...";
+    h4.innerHTML = "";
+    this.style.backgroundColor = "rgb(206, 38, 54)";
+    testBox.onclick = function () {
+        h1.innerHTML = "Wait For Green Please";
+        this.style.backgroundColor = "rgb(43, 135, 209)";
+        testBox.onclick = test;
+        clearTimeout(time);
+    };
+    let delay = Math.floor(Math.random() * 5000) + 1000;
+    let time = setTimeout(function () {
+        testBox.style.backgroundColor = "rgb(75, 219, 106)";
+        h1.innerHTML = "Click";
+        const t0 = performance.now();
+        testBox.onclick = function () {
+            h1.innerHTML = `${Math.trunc(performance.now() - t0)} ms`;
+            testBox.onclick = test;
+        };
+    }, delay);
+};
