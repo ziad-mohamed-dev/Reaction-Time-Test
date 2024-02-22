cd82db29ca43d6cc027aead44a418edaaaa5bdc2
Hunter-Smsm <143844320+Hunter-Smsm@users.noreply.github.com>
2024-02-23 00:36:38 +0200

    Performance Increased

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ let testBox = document.querySelector(".test");
 let h1 = document.querySelector("h1");
 let h4 = document.querySelector("h4");
 
-testBox.onclick = function test() {
+testBox.onmousedown = function test() {
     h1.innerHTML = "Wait For Green...";
     h4.innerHTML = "";
     this.style.backgroundColor = "rgb(206, 38, 54)";
-    testBox.onclick = function () {
+    testBox.onmousedown = function () {
         h1.innerHTML = "Wait For Green Please";
         this.style.backgroundColor = "rgb(43, 135, 209)";
-        testBox.onclick = test;
+        testBox.onmousedown = test;
         clearTimeout(time);
     };
     let delay = Math.floor(Math.random() * 5000) + 1000;
@@ -17,9 +17,9 @@ testBox.onclick = function test() {
         testBox.style.backgroundColor = "rgb(75, 219, 106)";
         h1.innerHTML = "Click";
         const t0 = performance.now();
-        testBox.onclick = function () {
+        testBox.onmousedown = function () {
             h1.innerHTML = `${Math.trunc(performance.now() - t0)} ms`;
-            testBox.onclick = test;
+            testBox.onmousedown = test;
         };
     }, delay);
 };
